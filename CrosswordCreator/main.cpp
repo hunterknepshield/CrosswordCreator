b682ee095f68f75619d4d764fd307ab2806af8f7
Hunter Knepshield <theragingfatcat@gmail.com>
2017-04-02 15:27:23 -0400

    Initial Commit

---
+++ CrosswordCreator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CrosswordCreator
+//
+//  Created by Hunter Knepshield on 4/2/17.
+//  Copyright © 2017 Hunter Knepshield. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
